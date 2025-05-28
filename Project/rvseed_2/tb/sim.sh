e34079367ceb6ae5652914d6e2e299274f7e0555
pychego <614693063@qq.com>
2025-05-28 20:32:41 +0800

    清楚未知文件

--- Project/rvseed_2/tb/sim.sh
+++
@@ -1,8 +0,0 @@
-echo "Compilation starts"
-# define   -I for include path  -y for library path  -o for output file
-# $1 is the first argument passed to the script
-iverilog -I ../core -y ../core -o sim.out $1
-echo "Generate waveforms"
-vvp -n sim.out
-echo "View waveforms"
-gtkwave sim_out.vcd
--- tb/tinyriscv_soc_tb.v
+++ tb/tinyriscv_soc_tb.v
@@ -403,7 +403,7 @@ module tinyriscv_soc_tb;
 
     // sim timeout
     initial begin
-      #500000 $display("Time Out.");
+        #500000 $display("Time Out.");
         $finish;
     end
 
